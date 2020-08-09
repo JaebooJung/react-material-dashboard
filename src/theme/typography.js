a62605fe52d1fb705315106925d4146ea4bae951
JaebooJung <rainhart0623@gmail.com>
2020-08-09 21:45:18 +0900

    Update font family

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -1,6 +1,7 @@
 import palette from './palette';
 
 export default {
+  fontFamily: `"Roboto", "Helvetica", "Arial", NanumGothic, serif`,
   h1: {
     color: palette.text.primary,
     fontWeight: 500,
